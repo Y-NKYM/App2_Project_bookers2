2d678b2b2215bd3f64b96633a0a0e82109bf83a0
YukiNakayama <121099971+Y-NKYM@users.noreply.github.com>
2023-06-06 02:10:36 +0000

    [Add] 新規登録・ログイン・ログアウトの機能追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_premitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_premitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+  end
 end
