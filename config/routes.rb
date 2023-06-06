dc1c3d7167f292a7df9dac2396369a6836055c73
YukiNakayama <121099971+Y-NKYM@users.noreply.github.com>
2023-06-06 08:27:56 +0000

    [Add] usersコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/edit'
+  get 'users/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'about' => 'homes#about', as: 'about'
   devise_for :users
