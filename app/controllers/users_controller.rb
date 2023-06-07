1ba35d04c43d43d722ba6e48022732003e94e466
YukiNakayama <121099971+Y-NKYM@users.noreply.github.com>
2023-06-07 11:05:16 +0000

    [Add] ユーザーに関するindex・show・edit・update機能の追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,25 @@
 class UsersController < ApplicationController
-  def edit
+  def index
+    @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
+	  @books = @user.books
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to user_path(user.id)
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :image, :introduction)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/edit'
-  get 'users/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'about' => 'homes#about', as: 'about'
   devise_for :users
   root to: 'homes#top'
   resources :books, only: [:create, :index, :show, :edit, :update, :destroy]
+  resources :users, only: [:index, :show, :edit, :update]
 end
