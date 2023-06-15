7e9ca71f917d3660d28d7c14d40c0103b8096a73
YukiNakayama <121099971+Y-NKYM@users.noreply.github.com>
2023-06-15 03:57:28 +0000

    [Add] いいね機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   resources :books, only: [:create, :index, :show, :edit, :update, :destroy] do
     resources :book_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
   end
   resources :users, only: [:index, :show, :edit, :update]
 end
