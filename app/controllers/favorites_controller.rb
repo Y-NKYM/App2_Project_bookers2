fd122073ff8914e3a3477ea0f546b63eafaa7b00
YukiNakayama <121099971+Y-NKYM@users.noreply.github.com>
2023-06-19 08:52:09 +0000

    [Add] いいね機能非同期処理エラーのコメント・flashメッセージBootstrap追加

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,7 +3,9 @@ class FavoritesController < ApplicationController
     @book = Book.find(params[:book_id])
     favorite = Favorite.new(book_id: @book.id)
     favorite.user_id = current_user.id
+    # ブラウザバックによりいいねが反映されていない場合エラーメッセージを出す。
     if Favorite.exists?(book_id: @book.id, user_id: current_user.id)
+      # error.js.erbを通して_favorite_error.htmlが読み込まれる。
       flash[:error] = "The page is not up to date. Please reload page."
       render :error
     else
@@ -16,10 +18,12 @@ class FavoritesController < ApplicationController
   def destroy
     @book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: @book.id)
+    # ブラウザバックにより反映されず、favorite内がnilの場合エラーメッセージを出す。
     unless favorite == nil
       favorite.destroy
       render :destroy
     else
+      # error.js.erbを通して_favorite_error.htmlが読み込まれる。
       flash[:error] = "The page is not up to date. Please reload page."
       render :error
     end
