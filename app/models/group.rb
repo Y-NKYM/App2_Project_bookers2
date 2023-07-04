486c48475767575f94ecfb7ec8bd697af39d65a3
YukiNakayama <121099971+Y-NKYM@users.noreply.github.com>
2023-07-04 14:27:45 +0000

    [Finish] 応用課題7c controller, view

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,5 +3,16 @@ class Group < ApplicationRecord
   has_many :group_users
   has_many :users, through: :group_users, dependent: :destroy
 
-  validates: :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true
+
+  def get_group_image(width, height)
+    unless group_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      group_image.attach(
+        io: File.open(file_path),
+        filename: 'default-image.jpg',
+        content_type: 'image/jpeg')
+    end
+    group_image.variant(resize_to_limit: [width, height]).processed
+  end
 end
